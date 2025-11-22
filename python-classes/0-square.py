f5dcda1c9b4206228613ac993d65749e6aca813e
root <root@kali>
2025-11-23 00:30:22 +0400

    1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""Module that defines a Square class."""
+
 
 class Square:
-    pass
+    """Class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize a square with a private size attribute.
+
+        Args:
+            size: The size of the square (no type/value validation).
+        """
+        self.__size = size
+
